0f77f087612d5853ac061a93387b8557110fab54
snehithlal <pssnehithlal@gmail.com>
2022-03-14 17:00:33 +0530

    feat: added assigned contacts and tag field in the form

---
+++ app/javascript/src/components/Dashboard/Notes/Pane/constants.js
@@ -0,0 +1,13 @@
+export const ASSIGNED_CONTACT_OPTIONS = [
+  { value: 1, label: "Ronald Richards" },
+  { value: 2, label: "Jacob Jones" },
+];
+
+export const TAG_OPTIONS = [
+  { value: 1, label: "Getting Started" },
+  { value: 2, label: "Onboarding" },
+  { value: 3, label: "User Flow" },
+  { value: 4, label: "UX" },
+  { value: 5, label: "Bugs" },
+  { value: 6, label: "V2" },
+];
